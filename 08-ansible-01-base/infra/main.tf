f22dd91097cec8b2ca865ae0bdcbbd164ab8b347
Aleksandr Ignatyev <vigntom@gmail.com>
2024-02-15 02:51:25 +0300

    Добавил контейнер fedora и рефакторинг создания ресурсов

--- 08-ansible-01-base/infra/main.tf
+++ 08-ansible-01-base/infra/main.tf
@@ -1,19 +1,13 @@
-resource "docker_image" "ubuntu" {
-  name = "vigntom/ubuntu-20.04"
+resource "docker_image" "image" {
+  count = length(local.containers)
+
+  name = local.containers[count.index].image
 }
 
-resource "docker_image" "centos7" {
-  name = "centos:7"
-}
+resource "docker_container" "container" {
+  count = length(local.containers)
 
-resource "docker_container" "ubuntu" {
-  image   = docker_image.ubuntu.image_id
-  name    = "ubuntu"
-  command = ["/bin/sleep", "infinity"]
-}
-
-resource "docker_container" "centos7" {
-  image = docker_image.centos7.image_id
-  name  = "centos7"
+  name = local.containers[count.index].name
+  image = docker_image.image[count.index].image_id
   command = ["/usr/bin/sleep", "infinity"]
 }
